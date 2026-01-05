79ceb41b46e29e1bc55c9486f5ce0030fb48c32b
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2026-01-05 12:50:02 +0530

    latest

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include<QDebug>
+#include<QTimer>
 
 int main(int argc, char *argv[])
 {
@@ -16,5 +17,12 @@ int main(int argc, char *argv[])
     // If you do not need a running Qt event loop, remove the call
     // to a.exec() or use the Non-Qt Plain C++ Application template.
     qDebug() << "First QT Program";
+    qDebug() << "Second QT Program";
+
+    QTimer::singleShot(2000,[](){
+        qDebug()<<"Timer Fired";
+        QCoreApplication::quit();
+    });// the block of code will only execute after two seconds and it moves line 27 even before 2 seconds
+
     return a.exec(); // starts the event loop
 }
