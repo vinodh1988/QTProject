ae8af63fe09eb51a645006466d7da4784a2a1725
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2026-01-09 11:28:37 +0530

    anotherversion

--- qfilewrite.cpp
+++ qfilewrite.cpp
@@ -2,24 +2,34 @@
 #include <QTextStream>
 #include <QFile>
 #include <QString>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QString sno="123";
-    QString name="Roger";
-    QString city="Ranchi";
 
+    QTextStream cin(stdin);
+    QTextStream cout(stdout);
+
+    cout << "Enter Sno: ";
+    cout.flush();
+    QString sno = cin.readLine();
+
+    cout << "Enter Name: ";
+    cout.flush();
+    QString name = cin.readLine();
+
+    cout << "Enter City: ";
+    cout.flush();
+    QString city = cin.readLine();
 
     QFile file("d:/datafiles/storex.txt");
     if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
         QTextStream out(&file);
         out << sno << "," << name << "," << city << "\n";
         file.close();
-        qDebug() << "Data written to file.\n";
+        cout << "Data written to file.\n";
     } else {
-        qDebug() << "Failed to open file for writing.\n";
+        cout << "Failed to open file for writing.\n";
     }
 
     return 0;
