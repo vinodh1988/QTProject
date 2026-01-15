01d63498ed6a3260542ff2aab4f95eb38c3389e4
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2026-01-15 10:43:43 +0530

    latest

--- mapsimple.cpp
+++ mapsimple.cpp
@@ -5,22 +5,22 @@
 #include <QThread>
 
 // Function to be applied to each element
-int square(int x) {
+void square(int &x) {
     qDebug() << "Executed by" << QThread::currentThread() << " number processed is " << x;
-    return x * x;
+    x = x * x;
 }
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
-    QVector<int> numbers = {1, 2, 3, 4, 5};
+    QVector<int> numbers = {1, 2, 3, 4, 5,34,5,5,7,8,4};
 
     // Use QtConcurrent::map to apply 'square' to each element
     // Note: map modifies in-place, but for demonstration, let's use mapped for a new container
-    QFuture<void> future = QtConcurrent::mapped(numbers, square);
+    QFuture<void> future = QtConcurrent::map(numbers, square);
 
     future.waitForFinished();
 
-
+    qDebug() << "After process" << numbers;
     return 0;
 }
